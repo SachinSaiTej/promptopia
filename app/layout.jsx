a27587f0977581a2c84f01557367700fa40671d1
SachinSaiTej <saitejakm2001@gmail.com>
2024-10-16 20:10:31 +0530

    Added db connection and OAuth and stored the user details in the db.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,24 @@
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 import "@styles/globals.css";
 
 export const metadata = {
   title: "Promptopia",
   description: "Discover & Share AI Prompts",
 };
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient"></div>
-        </div>
-        <main className="app">{children}</main>
+        <Provider>
+          <div className="main">
+            <div className="gradient"></div>
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
